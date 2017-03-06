cbbcba8d64dbe52067544bee1189129d795571a6
sathish <sathish.ofcl@gmail.com>
2017-03-07 01:59:29 +0530

    charts height and width adjusted

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,66 +1,96 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-  
+.controller('DashCtrl', function ($scope) {
 
-  // --------------------- Pie Chart Configuration -----------------------------
-  $scope.doughnutLabels = ["No Run", "Blocked", "Passed", "Failed"];
-  $scope.doughnutData = [122, 635, 1023, 784];
 
-  // --------------------- Line Chart Configuration ----------------------------
- 
-  $scope.barLabels = ["this is name of long project", "short name", "this is a medium sized name", "this is name of long project", "this is name of long project", "Short Name", "this is name of long project"];
-  $scope.options = {
+    // --------------------- Doughnut Chart Configuration -----------------------------
+    $scope.doughnutLabels = ["No Run", "Blocked", "Passed", "Failed"];
+    $scope.doughnutData = [122, 635, 1023, 784];
+    $scope.doughnutOptions = {
         responsive: true,
         maintainAspectRatio: false,
         barDatasetSpacing: 1,
         barShowStroke: true,
-        barStrokeWidth : 2,
-        barValueSpacing : 5,
-        showTooltips: false
+        barStrokeWidth: 2,
+        barValueSpacing: 5,
+        showTooltips: false,
+        legend: {
+            display: true,
+            position: 'top'
+        }
+    };
+
+    // --------------------- Line Chart Configuration ----------------------------
+
+    $scope.barLabels = ["this is name of long project", "short name", "this is a medium sized name", "this is name of long project", "this is name of long project", "Short Name", "this is name of long project"];
+    $scope.options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        barDatasetSpacing: 1,
+        barShowStroke: true,
+        barStrokeWidth: 2,
+        barValueSpacing: 5,
+        showTooltips: false,
+        legend: {
+            display: true,
+            position: 'top'
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    autoSkip: false,
+                    maxRotation: 90,
+                    minRotation: 90
+                }
+                    }]
+        }
     };
 
 
-  $scope.x = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  $scope.barSeries = ['Executed', 'Pending'];
+    $scope.x = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.barSeries = ['Executed', 'Pending'];
 
-  $scope.barData = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
+    $scope.barData = [
+    [65, 59, 80, 81, 56, 55, 40]
+
   ];
-// ---------Pie Chart---------------------
-    
-$scope.pieLabels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  $scope.pieData = [300, 500, 100];
-    
-$scope.pieOptions = {
+    // ---------Pie Chart---------------------
+
+    $scope.pieLabels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+    $scope.pieData = [300, 500, 100];
+
+    $scope.pieOptions = {
         responsive: true,
         maintainAspectRatio: false,
         barDatasetSpacing: 1,
         barShowStroke: true,
-        barStrokeWidth : 2,
-        barValueSpacing : 5,
-        showTooltips: false
+        barStrokeWidth: 2,
+        barValueSpacing: 5,
+        showTooltips: false,
+        legend: {
+            display: true,
+            position: 'top'
+        }
     };
 
-  
+
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+.controller('ChatsCtrl', function ($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    };
 })
 
-.controller('ChatDetailCtrl', function($scope, Chats, $stateParams) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function ($scope, Chats, $stateParams) {
+    $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('SocialCtrl', function($scope, Socials, $stateParams) {
-  $scope.items = Socials.all();
+.controller('SocialCtrl', function ($scope, Socials, $stateParams) {
+    $scope.items = Socials.all();
 })
 
-.controller('AccountCtrl', function($scope, Accounts, $stateParams) {
-  $scope.items = Accounts.all();
+.controller('AccountCtrl', function ($scope, Accounts, $stateParams) {
+    $scope.items = Accounts.all();
 });
